21466ad777cc9c4a5dc7b7666937e188870b5171
Anton Arnautov <arnautov.anton@gmail.com>
2025-10-21 16:13:22 +0200

    Initial setup

---
+++ packages/react-sdk/vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import { name, peerDependencies } from "./package.json";
+
+const external = [
+	// ...Object.keys(dependencies ?? {}),
+	...Object.keys(peerDependencies),
+];
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: {
+				index: resolve(import.meta.dirname, "./src/index.ts"),
+			},
+			fileName(format, entryName) {
+				return `${format}/${entryName}.${format === "cjs" ? "js" : "mjs"}`;
+			},
+			formats: ["es", "cjs"],
+			name,
+		},
+		emptyOutDir: false,
+		outDir: "dist",
+		minify: false,
+		sourcemap: true,
+		target: "es2020",
+		rollupOptions: {
+			external,
+		},
+	},
+});
