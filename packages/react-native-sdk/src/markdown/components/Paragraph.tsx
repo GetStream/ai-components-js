40eece92b15360a871bb1b7b6cc7b250134e7a45
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-10-31 17:35:56 +0100

    feat: port textual components as well

---
+++ packages/react-native-sdk/src/markdown/components/Paragraph.tsx
@@ -0,0 +1,74 @@
+import { Text, type TextStyle, View } from 'react-native';
+import type { MarkdownComponentProps, RuleRenderFunction } from '../types.ts';
+import { size, some } from 'lodash';
+import { useMemo } from 'react';
+
+export const Paragraph = ({
+  children,
+  styles,
+  node,
+  state,
+}: MarkdownComponentProps) => {
+  const paragraphStyle = useMemo(() => {
+    let style: TextStyle | (TextStyle | undefined)[] | undefined =
+      size(node.content) < 3 && some(node.content, { type: 'strong' })
+        ? styles.paragraphCenter
+        : styles.paragraph;
+
+    if (state.withinList) {
+      style = [style, styles.noMargin];
+    }
+
+    return style;
+  }, [
+    node.content,
+    state.withinList,
+    styles.paragraph,
+    styles.paragraphCenter,
+    styles.noMargin,
+  ]);
+
+  return <Text style={paragraphStyle}>{children}</Text>;
+};
+
+export const ParagraphWithImage = ({
+  children,
+  styles,
+}: MarkdownComponentProps) => (
+  <View style={styles.paragraphWithImage}>{children}</View>
+);
+
+export const renderParagraph: RuleRenderFunction = ({
+  node,
+  output,
+  state,
+  styles,
+}) => {
+  if (some(node.content, { type: 'image' })) {
+    state.withinParagraphWithImage = true;
+
+    return (
+      <ParagraphWithImage
+        key={state.key}
+        node={node}
+        output={output}
+        state={state}
+        styles={styles}
+      >
+        {output(node.content, state)}
+      </ParagraphWithImage>
+    );
+  }
+
+  return (
+    <Paragraph
+      key={state.key}
+      node={node}
+      output={output}
+      state={state}
+      styles={styles}
+    >
+      {output(node.content, state)}
+    </Paragraph>
+  );
+};
