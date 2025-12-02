ce67d9908f706484e58c72d545474b3bf32721eb
Anton Arnautov <arnautov.anton@gmail.com>
2025-12-02 15:28:25 +0100

    Post-rebase fixes

--- examples/react-example/src/components/MessageInputBar/MessageInputBar.tsx
+++ examples/react-example/src/components/MessageInputBar/MessageInputBar.tsx
@@ -1,20 +1,60 @@
 import { AIMessageComposer } from '@stream-io/chat-react-ai';
+import { useEffect } from 'react';
+import { isImageFile, type Channel, type UploadRequestFn } from 'stream-chat';
 import {
+  useAttachmentsForPreview,
   useChannelActionContext,
   useChannelStateContext,
+  useChatContext,
   useMessageComposer,
 } from 'stream-chat-react';
 import { startAiAgent } from '../../api.ts';
 import './MessageInputBar.scss';
 
+const isWatchedByAI = (channel: Channel) => {
+  return Object.keys(channel.state.watchers).some((watcher) =>
+    watcher.startsWith('ai-bot'),
+  );
+};
+
 export const MessageInputBar = () => {
+  const { client } = useChatContext();
   const { updateMessage, sendMessage } = useChannelActionContext();
   const { channel } = useChannelStateContext();
   const composer = useMessageComposer();
 
+  const { attachments } = useAttachmentsForPreview();
+
+  useEffect(() => {
+    if (!composer) return;
+
+    const upload: UploadRequestFn = (file) => {
+      const f = isImageFile(file) ? client.uploadImage : client.uploadFile;
+
+      return f.call(client, file as File);
+    };
+
+    const previousDefault = composer.attachmentManager.doDefaultUploadRequest;
+
+    composer.attachmentManager.setCustomUploadFn(upload);
+
+    return () => composer.attachmentManager.setCustomUploadFn(previousDefault);
+  }, [client, composer]);
+
   return (
     <div className="ai-demo-message-input-bar">
       <AIMessageComposer
+        onChange={(e) => {
+          const input = e.currentTarget.elements.namedItem(
+            'attachments',
+          ) as HTMLInputElement | null;
+
+          const files = input?.files ?? null;
+
+          if (files && files.length > 0) {
+            composer.attachmentManager.uploadFiles(files);
+          }
+        }}
         onSubmit={async (e) => {
           const event = e;
           const target = (event.currentTarget ??
@@ -23,39 +63,67 @@ export const MessageInputBar = () => {
 
           const formData = new FormData(event.currentTarget);
 
-          const t = formData.get('message');
+          const message = formData.get('message');
           const model = formData.get('model');
 
-          composer.textComposer.setText(t as string);
+          composer.textComposer.setText(message as string);
 
-          const d = await composer.compose();
+          const composedData = await composer.compose();
 
-          if (!d) return;
+          if (!composedData) return;
 
           target?.reset();
           composer.clear();
 
-          if (channel.initialized) {
-            const isAiAgentActive = Object.keys(channel.state.watchers).some(
-              (userId) => userId.startsWith('ai-bot'),
-            );
-            if (!isAiAgentActive) {
-              await startAiAgent(channel, model);
-            }
+          updateMessage(composedData?.localMessage);
 
-            await sendMessage(d);
-          } else {
-            updateMessage(d?.localMessage);
-
+          if (!channel.initialized) {
             await channel.watch();
-
-            // TODO: wrap in retry (in case channel creation takes longer)
-            await startAiAgent(channel, model);
-
-            await sendMessage(d);
           }
+
+          if (!isWatchedByAI(channel)) {
+            await startAiAgent(channel, model);
+          }
+
+          await sendMessage(composedData);
         }}
-      />
+      >
+        <AIMessageComposer.AttachmentPreview>
+          {attachments.map(({ localMetadata, thumb_url }) => (
+            <AIMessageComposer.AttachmentPreview.Item
+              key={localMetadata.id}
+              file={localMetadata.file as File}
+              state={localMetadata.uploadState}
+              imagePreviewSource={
+                thumb_url || (localMetadata.previewUri as string)
+              }
+              onDelete={() => {
+                composer.attachmentManager.removeAttachments([
+                  localMetadata.id,
+                ]);
+              }}
+              // TODO: onRetry
+            />
+          ))}
+        </AIMessageComposer.AttachmentPreview>
+        <AIMessageComposer.TextInput name="message" />
+        <div
+          style={{
+            display: 'flex',
+            gap: '1rem',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ display: 'flex', gap: '.25rem', alignItems: 'center' }}>
+            <AIMessageComposer.FileInput name="attachments" />
+            <AIMessageComposer.SpeechToTextButton />
+            <AIMessageComposer.ModelSelect name="model" />
+          </div>
+
+          <AIMessageComposer.SubmitButton />
+        </div>
+      </AIMessageComposer>
     </div>
   );
 };
